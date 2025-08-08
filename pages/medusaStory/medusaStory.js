df5c3d3ee1e3388dfbe7c4d240338b398f65c2f7
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-08-08 10:56:20 +0100

    removes image on hover

--- pages/medusaStory/medusaStory.js
+++ pages/medusaStory/medusaStory.js
@@ -1,18 +1,15 @@
 const backgroundBox = document.getElementById("backgroundBox");
-const bearImage = 'url("../../assets/medusa/urso.jpg")';
-  const bearBox = document.getElementById("bearBox");
-
+const bearUrl = 'url("../../assets/medusa/urso.jpg")';
+const bearBox = document.getElementById("bearBox");
+const bearImage = document.getElementById("bearImage");
 
 bearBox.addEventListener("mouseenter", () => {
-  console.log("entered");
-  bearBox.style.backgroundColor = "red";
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/bibbCountyAlabama.jpg")';
+  bearImage.remove();
 });
 
 bearBox.addEventListener("mouseleave", () => {
-  console.log("left");
-  bearBox.style.backgroundColor = "yellow";
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/fundoMeio.png")';
 });
