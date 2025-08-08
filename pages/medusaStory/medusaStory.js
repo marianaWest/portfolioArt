12ecd062685afa9e1405ad90e072c56e85c5bf19
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-08-08 10:49:30 +0100

    first commit

--- pages/medusaStory/medusaStory.js
+++ pages/medusaStory/medusaStory.js
@@ -2,22 +2,6 @@ const backgroundBox = document.getElementById("backgroundBox");
 const bearImage = 'url("../../assets/medusa/urso.jpg")';
   const bearBox = document.getElementById("bearBox");
 
-// window.addEventListener("DOMContentLoaded", () => {
-
-
-//   if (!bearBox) {
-//     console.error("bearBox not found");
-//     return;
-//   }
-
-//   bearBox.style.backgroundImage = bearImage;
-//   bearBox.style.height = "500px";
-
-//   bearBox.style.border = "4px solid transparent";
-//   bearBox.style.mask =
-//     "radial-gradient(#000 0 0), radial-gradient(#0000, #000 99%) padding-box";
-//   bearBox.style.maskComposite = "exclude";
-// });
 
 bearBox.addEventListener("mouseenter", () => {
   console.log("entered");
