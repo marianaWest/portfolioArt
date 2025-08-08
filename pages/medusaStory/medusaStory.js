27af17bf5c9b8fce5b87cfe2de38e8733be3c7e9
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-08-08 11:09:42 +0100

    image changes on hover

--- pages/medusaStory/medusaStory.js
+++ pages/medusaStory/medusaStory.js
@@ -1,15 +1,18 @@
 const backgroundBox = document.getElementById("backgroundBox");
-const bearUrl = 'url("../../assets/medusa/urso.jpg")';
 const bearBox = document.getElementById("bearBox");
 const bearImage = document.getElementById("bearImage");
+const originalBearSrc = bearImage.src;
+const newBearSrc = "../../assets/medusa/medusa.jpg";
 
 bearBox.addEventListener("mouseenter", () => {
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/bibbCountyAlabama.jpg")';
-  bearImage.remove();
+  bearImage.src = newBearSrc;
+  //   bearImage.remove();
 });
 
 bearBox.addEventListener("mouseleave", () => {
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/fundoMeio.png")';
+  bearImage.src = originalBearSrc;
 });
