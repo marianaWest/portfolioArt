236dd4f438aa9188446a86975db1e598f6aa6488
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-08-13 17:08:47 +0100

    Included js for caption

--- pages/medusaStory/medusaStory.js
+++ pages/medusaStory/medusaStory.js
@@ -1,15 +1,18 @@
 const backgroundBox = document.getElementById("backgroundBox");
 const bearBox = document.getElementById("bearBox");
 const bearImage = document.getElementById("bearImage");
+const caption = document.getElementById("caption");
 const originalBearSrc = bearImage.src;
 
 bearBox.addEventListener("mouseenter", () => {
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/bibbCountyAlabama.jpg")';
+  caption.style.visibility = "visible";
 });
 
 bearBox.addEventListener("mouseleave", () => {
   backgroundBox.style.backgroundImage =
     'url("../../assets/medusa/fundoMeio.png")';
   bearImage.src = originalBearSrc;
+  caption.style.visibility = "hidden";
 });
