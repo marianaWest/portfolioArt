fd7964e6374ddbfd9e38e92f1de2457b5f3cd784
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-09-09 16:19:37 +0100

    eye closes on click

--- pages/medusaStory/medusaStoryQueen/medusaStoryQueen.js
+++ pages/medusaStory/medusaStoryQueen/medusaStoryQueen.js
@@ -3,6 +3,8 @@ const eye1 = document.getElementById("eye1");
 const eye2 = document.getElementById("eye2");
 const rollText = document.getElementById("rollText");
 const rollText1 = document.getElementById("rollText1");
+const eyeClosedSrc = "../../../assets/closedEye.svg";
+const eye1Img = document.getElementById("eye1Img");
 
 window.addEventListener("load", () => {
   blackout.classList.remove("active");
@@ -10,6 +12,7 @@ window.addEventListener("load", () => {
 
 eye1.addEventListener("click", () => {
   rollText1.style.visibility = "visible";
+  eye1Img.src = eyeClosedSrc;
 });
 
 eye2.addEventListener("click", () => {
