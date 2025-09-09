e2434b9e384cdd6d63ada53eb3a66e64ce0ce6ea
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2025-09-09 14:51:44 +0100

    Included eye picture

--- pages/medusaStory/medusaStoryQueen/medusaStoryQueen.js
+++ pages/medusaStory/medusaStoryQueen/medusaStoryQueen.js
@@ -1,5 +1,12 @@
 const blackout = document.getElementById("blackout");
+const eye1 = document.getElementById("eye1");
+const rollText1 = document.getElementById("rollText1");
 
 window.addEventListener("load", () => {
   blackout.classList.remove("active");
+});
+
+eye1.addEventListener("click", () => {
+  rollText1.style.visibility = "visible";
+  console.log("clicked");
 });
